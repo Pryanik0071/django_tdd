2b68f2b5e73ceff4f7c2e0a1769ab5735e5ead2e
Dmitry M. <di.mesler.2010@yandex.ru>
2026-02-02 16:54:22 +0300

    Add: unittest case

--- backend/functional_tests.py
+++ backend/functional_tests.py
@@ -1,6 +1,31 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'The install worked successfully! Congratulations!' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """Тест нового посетителя"""
+
+    def setUp(self):
+        """Установка"""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """Демонтаж"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """Тест: можно начать список и получить его позже"""
+        # Эдит слышала про крутое новое онлайн-приложение со
+        # списком неотложных дел. Она решает оценить его
+        # домашнюю страницу
+        self.browser.get('http://localhost:8000')
+
+        # Она видит, что заголовак и шапка страницы говорят о
+        # списках неотложных дел
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
