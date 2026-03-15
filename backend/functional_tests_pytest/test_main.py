ea06752100043a9f1142524908703611a530ae55
Dmitry M. <di.mesler.2010@yandex.ru>
2026-03-15 19:58:12 +0300

    Update: pytests

--- backend/functional_tests_pytest/test_main.py
+++ backend/functional_tests_pytest/test_main.py
@@ -108,3 +108,25 @@ def test_multiple_users_can_start_lists_at_different_urls(browser, live_server):
         # Удовлетворенные, они оба ложатся спать
     finally:
         browser.quit()
+
+
+def test_layout_and_styling(browser, live_server):
+    """Макет и стилевое оформление"""
+    # Эдит открывает домашнюю страницу
+    browser.get(live_server.url)
+    browser.set_window_size(1024, 768)
+
+    # Она замечает, что поле ввода аккуратно центрированно
+    input_box = browser.find_element(By.ID, 'id_new_item')
+    assert abs(
+        input_box.location['x'] + input_box.size['width'] / 2 - 512
+    ) < 10
+
+    # Она начинает новый список и видит, что поле ввода там тоже центрировано
+    input_box.send_keys('testing')
+    input_box.send_keys(Keys.ENTER)
+    wait_for_row_in_list_table(browser, '1: testing')
+    input_box = browser.find_element(By.ID, 'id_new_item')
+    assert abs(
+        input_box.location['x'] + input_box.size['width'] / 2 - 512
+    ) < 10
