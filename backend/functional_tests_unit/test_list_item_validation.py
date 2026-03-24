a1a50ba4d4a84234954699f636164c7403c8d82f
Dmitry M. <di.mesler.2010@yandex.ru>
2026-03-24 22:38:37 +0300

    Refactoring: tests

--- backend/functional_tests_unit/test_list_item_validation.py
+++ backend/functional_tests_unit/test_list_item_validation.py
@@ -1,5 +1,8 @@
 from unittest import skip
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 from .base import FunctionalTest
 
 
@@ -11,16 +14,37 @@ class ItemCalidationTest(FunctionalTest):
         """Нельзя добавлять пустые элементы списка"""
         # Эдит открывает домашнюю страницу и случайно пытается отправить пустой элемент списка.
         # Она нажимает Enter на пустом поле ввода
+        self.browser.get(self.live_server_url)
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
 
         # Домашняя страница обновляется, и появляется сообщение об ошибке,
         # которое говорит, что элементы списка не должны быть пустыми
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, ".has-error").text,
+                "You can't have an empty list item",
+            )
+        )
 
         #  Она пробует снова, теперь с неким текстом для элемента, и теперь
         #  это срабатывает
+        self.browser.find_element(By.ID, "id_new_item").send_keys("Purchase milk")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Purchase milk")
 
         #  Как ни странно, Эдит решает отправить второй пустой элемент списка
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
 
         # Она получает аналогичное предупреждение на странице списка
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, ".has-error").text,
+                "You can't have an empty list item",
+            )
+        )
 
         # И она может его исправить, заполнив поле неким текстом
         self.fail('напиши меня!')
+        self.browser.find_element(By.ID, "id_new_item").send_keys("Make tea")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("2: Make tea")
